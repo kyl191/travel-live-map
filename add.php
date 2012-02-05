1dbbf922809c2c91761819b9692ee4fd1b22d7a9
kyl191 <github@kyl191.net>
2012-02-05 22:00:23 +0800

    Move stuff around so I can add other ways to  upload

--- add.php
+++ add.php
@@ -5,10 +5,23 @@
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true&libraries=geometry"></script>
 <script type="text/javascript" src="map.js"></script>
 <script type="text/javascript">
+    var latitude = 0, longitude = 0;
+    function saveCoords(){
+        $.ajax({
+            url: "save.php", 
+            data: {lat: latitude, long: longitude}, 
+            error: function(data){
+                alert("Error uploading: " + data);
+            }
+            success: function(data){
+                alert("Uploaded coordinates successfully!");
+            }
+        });
+    }
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position){
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
+        latitude = position.coords.latitude;
+        longitude = position.coords.longitude;
         var coords = new google.maps.LatLng(latitude, longitude);
         var mapOptions = {
             zoom: 15,
@@ -27,17 +40,9 @@
             });
 
         map.panTo(marker.getPosition());
-        $.ajax({
-            url: "save.php", 
-            data: {lat: latitude, long: longitude}, 
-            error: function(data){
-                alert("Error uploading: " + data);
-            }
+        saveCoords();
         });
-
- 
-        });
-    }else {
+    } else {
         // Do nothing.
     }
 	</script>
