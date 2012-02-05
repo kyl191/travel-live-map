8b6025750e5d55210ab7776dee3e71c2023073f7
kyl191 <github@kyl191.net>
2012-02-05 18:18:10 +0800

    JSON output of the coords and timestamp

--- load.php
+++ load.php
@@ -3,8 +3,10 @@ if($db){
 	$sql = 'SELECT lat,long,timestamp FROM points';
 $sth = $db->prepare($sql);
 $sth->execute();
-$result = $sth->fetchAll();
+$result = $sth->fetchAll(PDO::FETCH_ASSOC);
+echo json_encode($result);
 } else {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
 	echo "DB error.";
 }
 ?>
