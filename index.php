22921b5ca589f32a45dd52160bdfa4c4357a3913
kyl191 <github@kyl191.net>
2012-02-05 16:29:59 +0800

    Include the js file

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
 <head>
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+<script type="text/javascript" src="map.js"></script>
 </head>
 <body>
 
