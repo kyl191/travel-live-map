313062b59bc6597b20b85ee1eeba894612801cc9
kyl191 <github@kyl191.net>
2012-02-10 20:54:34 +0900

    A bunch of changes.

--- save.php
+++ save.php
@@ -1,16 +1,28 @@
+// Todo: 
+// - Check for password
+// - Allow timestamp to be specified (allows recorded updates)
+// - possibly use JSON for updates - json_encode/decode is probably trivial, though I'd need to see the array that gets sent... 
 <?php include("db.php");
 if ($_GET['password'] != "buns") {
 	header($_SERVER['SERVER_PROTOCOL'] . ' 403 Forbidden', true, 403);
 	die("Wrong password.")
 }
 if($db){
-	$sql = "INSERT INTO points (`lat`,`long`, `timestamp_old`) VALUES (:lat, :long, :timestamp)";
+	$sql = "INSERT INTO points (`lat`,`long`, `timestamp`, `timestamp_old`) VALUES (:lat, :long, :timestamp, :timestamp_old)";
 $sth = $db->prepare($sql);
 $sth->bindParam('lat', $_GET['lat']);
 $sth->bindParam('long', $_GET['long']);
-$date = new DateTime("now", new DateTimeZone("Asia/Singapore"));
-$sth->bindParam('timestamp', $date->format(DATE_RFC850));
-$result =$sth->execute();
+if ($_GET['timestamp'] == null){
+	$date = new DateTime("now", new DateTimeZone("Asia/Singapore"));
+	$sth->bindParam('timestamp_old', $date->format(DATE_RFC850));
+	$sth->bindValue('timestamp', null, PDO::PARAM_NULL);
+} else {
+	$date = new DateTime("now", new DateTimeZone("Asia/Singapore"));
+	$date->setTimestamp((int)$_GET['timestamp']);
+	$sth->bindParam('timestamp_old', $date->format(DATE_RFC850));
+	$sth->bindParam('timestamp', $_GET['timestamp']);
+}
+$result = $sth->execute();
 	if ($result) {
 		echo "OK";
 	} else {
